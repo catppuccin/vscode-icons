f75659248ac9e4a7d02ec7e8f00c2f39680d66d8
prazdevs <1631886+prazdevs@users.noreply.github.com>
2024-01-10 21:42:40 +0100

    chore: generation script

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,6 +1,5 @@
-import { readdir, writeFile, readFile } from "node:fs/promises"
+import { readdirSync, readFileSync, writeFileSync } from "node:fs"
 import { resolve } from 'node:path'
-
 import type { FlavorName, AccentName, MonochromaticName } from "@catppuccin/palette"
 import { flavorEntries } from '@catppuccin/palette'
 import { SVG, parseColors } from "@iconify/tools"
@@ -12,29 +11,30 @@ const palettes = flavorEntries.reduce((acc, [name, flavor]) => ({
     .filter(([color, { accent }]) => accent || ['text', 'overlay1'].includes(color))
     .map(([color, { hex }]) => [color, hex])
 }), {} as Record<FlavorName, Array<[ColorName ,string]>>)
-
 const flavors = flavorEntries.map(([f]) => f)
 
 for(const origin of flavors) {
   const originPath = resolve('icons', origin)
-  const originSvgs = await readdir(originPath)
+  const originSvgs = readdirSync(originPath)
 
   for (const dest of flavors.filter(f => f !== origin)) {
     const destPath = resolve('icons', dest)
-    const destSvgs = await readdir(destPath)
+    const destSvgs = readdirSync(destPath)
     originSvgs.filter(s => !destSvgs.includes(s)).forEach(async i => {
-      const svg = new SVG(await readFile(resolve(originPath, i), 'utf8'))
+      const svg = new SVG(readFileSync(resolve(originPath, i), 'utf8'))
       parseColors(svg, {
         callback(attr, color) {
           if (attr === "stroke") {
             const newColorName = palettes[origin].find(v => v[1] === color)?.[0]
+            if (!newColorName) 
+              throw new Error(`Color '${color}' found in '${i}' is not in ${origin} palette.`)
             const newColor = palettes[dest].find(v => v[0] === newColorName)?.[1]
             return newColor
           }
           return color
         }
       })
-      await writeFile(resolve(destPath, i), svg.toPrettyString())
+      writeFileSync(resolve(destPath, i), svg.toPrettyString())
     })
   }
 }
