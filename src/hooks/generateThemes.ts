31c08bee2790e156df2169b55373808147fb60e9
prazdevs <1631886+prazdevs@users.noreply.github.com>
2024-01-22 23:38:05 +0100

    feat: add file icons to themes

--- src/hooks/generateThemes.ts
+++ src/hooks/generateThemes.ts
@@ -5,6 +5,7 @@ import { flavorEntries } from '@catppuccin/palette'
 import { options } from '../defaults/options'
 import { baseIcons } from '../defaults/baseIcons'
 import { folderIconEntries } from '../defaults/folderIcons'
+import { fileIconEntries } from '../defaults/fileIcons'
 
 const root = join(__dirname, '../..')
 const flavors = flavorEntries.map(([f]) => f)
@@ -38,9 +39,32 @@ async function generateThemes() {
       },
     )
 
+    const fileIcons = fileIconEntries.reduce(
+      ({ languageIds, fileExtensions, fileNames }, [name, icon]) => ({
+        languageIds: {
+          ...languageIds,
+          ...icon.languageIds?.reduce((a, c) => ({ ...a, [c]: name }), {}),
+        },
+        fileExtensions: {
+          ...fileExtensions,
+          ...icon.fileExtensions?.reduce((a, c) => ({ ...a, [c]: name }), {}),
+        },
+        fileNames: {
+          ...fileNames,
+          ...icon.fileNames?.reduce((a, c) => ({ ...a, [c]: name }), {}),
+        },
+      }),
+      {
+        languageIds: {},
+        fileExtensions: {},
+        fileNames: {},
+      },
+    )
+
     const theme = {
       ...options,
       ...baseIcons,
+      ...fileIcons,
       ...folderIcons,
       iconDefinitions,
     }
