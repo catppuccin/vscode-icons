d9d255e9b94eba35e5732264a6e2503cb842c13c
prazdevs <1631886+prazdevs@users.noreply.github.com>
2024-01-22 23:37:44 +0100

    chore: lint defaults

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,17 @@ const config = require('@antfu/eslint-config').default
 
 module.exports = config({
   ignores: [],
+}, {
+  files: ['src/defaults/*.ts'],
+  rules: {
+    'sort-keys': ['error', 'asc', { minKeys: 10 }],
+    'style/array-bracket-newline': ['error', { minItems: 2 }],
+    'style/array-element-newline': ['error', 'always'],
+    'style/object-curly-spacing': ['error', 'always'],
+    'style/object-curly-newline': ['error', 'always'],
+    'style/object-property-newline': ['error', {
+      allowAllPropertiesOnSameLine: false,
+      allowMultiplePropertiesPerLine: false,
+    }],
+  },
 })
