6b4646dac71db4f544a51130a38a802687521563
uncenter <47499684+uncenter@users.noreply.github.com>
2024-08-31 17:03:30 -0400

    feat(associations): add `.python-version` to `python-config` icon (#272)

--- src/defaults/fileIcons.ts
+++ src/defaults/fileIcons.ts
@@ -1855,7 +1855,11 @@ const fileIcons: Record<string, {
     ],
   },
   'python-config': {
-    fileNames: ['pyproject.toml', 'requirements.txt'],
+    fileNames: [
+      'pyproject.toml',
+      'requirements.txt',
+      '.python-version',
+    ],
   },
   'r': {
     languageIds: [
