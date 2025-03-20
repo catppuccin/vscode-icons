cf87da134caa8660bdb763ce5e1b61471fda5a69
MuntasirSZN <muntasir.joypurhat@gmail.com>
2025-03-20 15:00:42 +0600

    refactor: another js to ts refactor

---
+++ scripts/build.ts
@@ -0,0 +1,43 @@
+import process from 'node:process'
+import { consola } from 'consola'
+import { generateFonts } from 'fantasticon'
+import packageJson from 'package.json' assert { type: 'json' }
+import codepoints from '../mapping.json'
+import { folders } from './icons/utils/palettes'
+
+function opts(flavor: string) {
+  return {
+    name: `catppuccin-code-icons`,
+    prefix: `catppuccin-${flavor}`,
+    codepoints,
+    inputDir: `./icons/${flavor}`,
+    outputDir: `./dist/${flavor}`,
+    fontTypes: ['ttf', 'woff', 'woff2'],
+    normalize: true,
+    assetTypes: ['css', 'html'],
+    formatOptions: {
+      ttf: {
+        url: packageJson.repository.url,
+        description: packageJson.description,
+        version: packageJson.fontVersion,
+      },
+    },
+  }
+}
+
+// Generate fonts for each flavor in the folders array
+async function generateAllFonts() {
+  for (const flavor of folders) {
+    consola.info(`Generating fonts for ${flavor} flavor...`)
+    /* eslint-disable-next-line ts/ban-ts-comment */
+    // @ts-ignore
+    await generateFonts(opts(flavor))
+    consola.success(`Generated fonts for ${flavor} flavor.`)
+  }
+}
+
+// Call the function to generate all fonts
+generateAllFonts().catch((error) => {
+  consola.error('Error generating fonts:', error)
+  process.exit(1)
+})
---
+++ scripts/icons/utils/palettes.ts
@@ -0,0 +1,43 @@
+import type {
+  AccentName,
+  FlavorName,
+  MonochromaticName,
+} from '@catppuccin/palette'
+import { flavorEntries, flavors } from '@catppuccin/palette'
+
+export type ColorName =
+  | AccentName
+  | Extract<MonochromaticName, 'text' | 'overlay1'>
+
+/**
+ * Simplified ColorName/Hexcode palettes
+ */
+export const palettes = {
+  ...flavorEntries.reduce(
+    (acc, [flavorName, flavor]) => ({
+      ...acc,
+      [flavorName]: flavor.colorEntries
+        .filter(
+          ([color, { accent }]) =>
+            accent || ['text', 'overlay1'].includes(color),
+        )
+        .map(([color, { hex }]) => [color, hex]),
+    }),
+    {} as Record<FlavorName | 'css-variables', Array<[ColorName, string]>>,
+  ),
+  'css-variables': flavors.latte.colorEntries
+    .filter(
+      ([color, { accent }]) => accent || ['text', 'overlay1'].includes(color),
+    )
+    .map(([color]) => [color, `var(--vscode-ctp-${color})`]) as Array<
+    [ColorName, string]
+  >,
+}
+
+export const folders = [
+  'css-variables',
+  'frappe',
+  'latte',
+  'macchiato',
+  'mocha',
+] satisfies Array<keyof typeof palettes>
