3b168c7f9e240afb7e5a6ad55a53a98dc984a603
Muntasir Mahmud <muntasir.joypurhat@gmail.com>
2025-03-23 04:50:20 +0600

    Update build.ts

--- scripts/build.ts
+++ scripts/build.ts
@@ -47,43 +47,43 @@ async function generateAllFonts() {
     await generateFonts(opts(flavor))
     await execa`pnpx woff2otf dist/${flavor}/catppuccin-code-icons.woff dist/${flavor}/catppuccin-code-icons.otf`
 
-      const config = {
-        mode: {
-          symbol: {
-            dest: path.join(outDir, flavor),
-            sprite: outFile,
-          },
+    const config = {
+      mode: {
+        symbol: {
+          dest: path.join(outDir, flavor),
+          sprite: outFile,
         },
+      },
+    }
+
+    const spriter = new SVGSpriter(config)
+
+    mappingEntries.forEach(([mappedName, symbol]) => {
+      const file = path.resolve(`./icons/${flavor}/${mappedName}.svg`)
+
+      if (fs.existsSync(file)) {
+        for (const name of findNames(symbol)) {
+          spriter.add(
+            path.resolve(`./icons/${flavor}/${name}.svg`),
+            `${name}.svg`,
+            fs.readFileSync(file, 'utf-8'),
+          )
+        }
+      }
+    })
+
+    spriter.compile((error, result) => {
+      if (error) {
+        consola.info(`Error compiling sprite for ${flavor}:`, error)
+        return
       }
 
-      const spriter = new SVGSpriter(config)
-
-      mappingEntries.forEach(([mappedName, symbol]) => {
-        const file = path.resolve(`./icons/${flavor}/${mappedName}.svg`)
-
-        if (fs.existsSync(file)) {
-          for (const name of findNames(symbol)) {
-            spriter.add(
-              path.resolve(`./icons/${flavor}/${name}.svg`),
-              `${name}.svg`,
-              fs.readFileSync(file, 'utf-8'),
-            )
-          }
-        }
-      })
-
-      spriter.compile((error, result) => {
-        if (error) {
-          consola.info(`Error compiling sprite for ${flavor}:`, error)
-          return
-        }
-
-        const outputDir = path.resolve(path.join(outDir, flavor))
+      const outputDir = path.resolve(path.join(outDir, flavor))
         fs.mkdirSync(outputDir, { recursive: true })
 
-        const outputPath = result.symbol.sprite.path
+      const outputPath = result.symbol.sprite.path
         fs.writeFileSync(outputPath, result.symbol.sprite.contents)
-      })
+    })
 
     consola.success(`Generated fonts for ${flavor} flavor.`)
   }
